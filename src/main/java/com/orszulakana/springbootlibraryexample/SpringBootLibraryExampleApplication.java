373c3ea950bcbc79d31db56216c2f0f6ee41abb2
orszulakana <orszulakana@gmail.com>
2018-08-31 18:32:58 +0200

    setup project

---
+++ src/main/java/com/orszulakana/springbootlibraryexample/SpringBootLibraryExampleApplication.java
@@ -0,0 +1,12 @@
+package com.orszulakana.springbootlibraryexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLibraryExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootLibraryExampleApplication.class, args);
+	}
+}
